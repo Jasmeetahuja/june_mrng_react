1094bda421089544b882455dcf13dd7e33b22923
JasmeetAhuja <jassideep013@gmail.com>
2019-06-14 00:24:30 +0530

    2nd

---
+++ firstapp/src/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Header =()=> {
+    return(
+        <div>
+            <center>
+              <h1>  React App</h1>
+            </center>
+        </div>
+    )
+}
+
+export default Header;
