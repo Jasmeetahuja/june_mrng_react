187acb406afc16369921b10e72a095794ffed663
JasmeetAhuja <jassideep013@gmail.com>
2019-07-12 18:12:51 +0530

    redux

--- redux1/src/index.js
+++ redux1/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
-import { Provider } from 'rea' 
+import MoviesList from './container/movies_list';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App/>, document.getElementById('root');
+
+
+ReactDOM.render(
+<Provider store ={store}>
+    <MoviesList/>
+</Provider>, document.getElementById('root'));
